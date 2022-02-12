b46dbd6f75c7ea9ad90062c1b32468e108fc0acc
CSSHTML098 <92036900+CSSHTML098@users.noreply.github.com>
2022-02-12 16:03:48 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,188 @@
+// // #1
+// let a = +prompt("сторона квадрата")
+// let b = a*4
+// alert("Р квадрата = " + b)
+// console.log(b)
+
+
+// #2
+// let a = +prompt("сторона квадрата")
+// let b = a*a
+// alert("S квадрата = " + b)
+// console.log(b)
+
+
+// #3
+// let a = +prompt("сторона а")
+// let b = +prompt("сторона b")
+// let S = a*b
+// let P = 2*(a+b)
+// alert("S прямоугольника = " + S)
+// console.log(S)
+// alert("P прямоугольника = " + P)
+// console.log(P)
+
+
+// #4
+// let d = +prompt("Диаметр окружности")
+// let L = 3.14 * d
+// alert("Длина окружности = " + L)
+// console.log(L)
+
+
+// #5
+// let a = +prompt("длина ребра")
+// let V = a*a*a
+// let S = 6*a*a
+// alert("V куьа = " + V)
+//console.log(V)
+// alert("S куба = " + S) 
+// console.log(S)
+
+
+// #6
+// let a = +prompt("a")
+// let b = +prompt("b")
+// let c = +prompt("c")
+// let V = a*b*c
+// let S = 2*(a*b + b*c + c*a)
+// alert("V = " + V)
+// console.log(V)
+// alert("S =" + S)
+// console.log(S)
+
+
+// #7
+// let R = +prompt("R окружности")
+// let L = 2*3.14*R
+// let S = 3.14*(R**2)
+// alert("L = " + L)
+// console.log(V)
+// alert("S = " + S)
+// console.log(S)
+
+
+// #8
+// let a = +prompt("a")
+// let b = +prompt("b") 
+// let c = (a+b)/2
+// alert("Среднее арефметическое данных чисел = " + c)
+// console.log(c)
+
+
+// #9 
+// let a = +prompt("a")
+// let b = +prompt("b")
+// let c = (a*b)**0.5
+// alert("Среднее геометрическое данных числел = " + c)
+// console.log(c)
+
+
+// #10
+// let a = +prompt ("Введите число")
+// let b = +prompt ("Введите второе число")
+// let c = (a**2)+(b**2)
+// let d = (a**2)-(b**2)
+// let e = (a**2)*(b**2)
+// let f = (a**2)/(b**2)
+// alert(" Сумма квадратов =" + c + " Разность квадаратов =" + d + " Произведение квадратов =" + e + " Частное квадратов =" + f) 
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+
+
+// #11
+// let a = +prompt ("Введите число")
+// let b = +prompt ("Введите второе число")
+// let c = ((a**2)**0.5)+((b**2)**0.5)
+// let d = ((a**2)**0.5)-((b**2)**0.5)
+// let e = ((a**2)**0.5)*((b**2)**0.5)
+// let f = ((a**2)**0.5)/((b**2)**0.5)
+// alert(" Сумма модулей = " + c + " Разность модулей = " + d + " Произведение модулей = " + e + " Частное модулей = " + f) 
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+
+
+// #12
+/*let a = +prompt("Введите первый катет")
+let b = +prompt("Введите второй катет")
+let gipotinyza = ((a**2)+(b**2))**0.5
+let c = gipotinyza
+let p = a+b+c
+alert("Гипотенуза = " + c + ";" + "   P = " + p + ".")
+console.log(c)
+console.log(p)*/
+
+
+// #13
+/*alert("При решении данной задачи радиус первого круга должен быть больше радиуса второго")
+let r1 = +prompt("Введите радиус первого круга")
+let r2 = +prompt("Введите радиус второго круга круга")
+let s1 = 3.14*(r1**2)
+let s2 = 3.14*(r2**2)
+let s3 = s1-s2
+alert("S первого круга = " + s1 + ";" + "    S второго круга = " + s2 + ";" + "    S кольца = " + s3 + ".")
+console.log(s1)
+console.log(s2)
+console.log(s3)*/
+
+
+// #14
+/*let l = +prompt("Введите длинну окружности")
+let r = l/(2*3.14)
+let s = 3.14*(r**2)
+alert("Радиус окружности = " + r + ";" + "   Её площадь = " + s + ".")
+console.log(r)
+console.log(s)*/
+
+
+// #15
+/*let s = +prompt("Введите S круга")
+let d = ((4*s)/3.14)**0.5
+let l = 3.14*d
+alert("D круга = " + d + ";" + "   L круга = " + l + ".")
+console.log(d)
+console.log(l)*/
+
+
+// #25
+/*let x = prompt("Введите x")
+let y = 3*(x**6) - 6*(x**2) - 7
+alert(y)
+console.log(y)*/
+
+
+// #26
+/*let x = +prompt("Введите значение аргумента")
+let y = (4*((x-3)**6))-(7*((x-3)**3))-2
+alert("Значение функции = " + y)
+console.log(y)*/
+
+
+// #27
+/*let a = +prompt("Введите число")
+let b = a**2
+let с = a**4
+let d = a**8
+alert("Данное число во 2 степени = " + b + ";" + "   Данное число в 4 степени = " + c + ";" + "   Данное число в 8 степени = " + d + ".")
+console.log(b)
+console.log(c)
+console.log(d)*/
+
+
+// #28
+/*let a = +prompt("Введите число")
+let b = a**2
+let с = a**3
+let d = a**5
+let e = a**10
+let f = a**15
+alert("Данное число во 2 степени = " + b + ";" + "   Данное число в 3 степени = " + c + ";" + "   Данное число в 5 степени = " + d + ";" + "Данное число в 3 степени = " + e + ";" + "    Данное число в 5 степени = " + f + ".")
+console.log(b)
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)*/
